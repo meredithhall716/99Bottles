69da8d02a2fecc5b06644718ba7270d8fab0259b
meredithhall716 <meredith.hall.oboe@gmail.com>
2022-05-30 18:01:48 -0400

    first

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,16 @@
 function bottleSong() {
-  // Write your code here!
-};
+  let num = 99
+    
+  while (num >= 1) {
+    if (num === 2) {
+      console.log (num + " bottles of beer on the wall. " + num + " bottles of beer. \n" + "Take one down and pass it around, " + (num - 1) + " bottle of beer on the wall.")
+    } else if (num === 1) {
+      console.log("1 bottle of beer on the wall, 1 bottle of beer. \n" + "Take one down and pass it around, no more bottles of beer on the wall." + " No more bottles of beer on the wall, no more bottles of beer.\n" + "Go to the store and buy some more, 99 bottles of beer on the wall.")
+    } else {
+      console.log(num + " bottles of beer on the wall. " + num + " bottles of beer. \n" + "Take one down and pass it around, " + (num - 1) + " bottles of beer on the wall." )
+    }
+    num--
+  }
+}
 
 bottleSong();
